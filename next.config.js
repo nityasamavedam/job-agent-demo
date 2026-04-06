20ce461f0b2ac5878c990637ecece5bac2f60f1d
Nitya Samavedam <nitya.samavedam@gmail.com>
2026-04-06 16:30:43 +0530

    Add serverExternalPackages config for pdf-parse, mammoth, formidable

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverExternalPackages: ['formidable', 'pdf-parse', 'mammoth'],
+}
 module.exports = nextConfig
